3da593a5aec2beaae57077f9c305157d7ad222a9
Soner Mezgitci <mezgitci9@gmail.com>
2020-06-16 12:37:38 -0400

    refactored updated

--- index.js
+++ index.js
@@ -1,64 +1,30 @@
-
-// JS mantra When the DOM CONTENT is loaded i want to make a get Fetch and then manipulate DOM content Loaded 
-// with render data with renderUsers(user),method below 
 addEventListener('DOMContentLoaded',() => {
-  // Creating DOMCONTENTLOADED event listener to properly set up listener
-  getUsers() //its for get users on the DOM for  Fetch Read Test! 
+ 
+  getUsers() 
 
   const createUsersForm =  document.querySelector("#new-user-form")
   createUsersForm.addEventListener("submit",(e) => createFormHandler(e))
   let displayNumberDiv = document.querySelector("#display-number")
   
-  
-  
-  function getUsers(){ // we are creating a function to have FETCH and we passed it On DOMContent loaded)
-    fetch("http://localhost:3000/users")// return the promise has response to take out 
-    .then(r => r.json()) // (r) capturing this respond and parse in the  json 
+  function getUsers(){ 
+    fetch("http://localhost:3000/users") 
+    .then(r => r.json()) 
     .then(users => users.forEach(user => {  
-      
-      //  debugger 
-      let newUser = new User(user)
-      //each new instances i am going tp push it this(user"first user") 
-      // because i create new intsances in my user class everytime i create new instance it goes to my constructor in user.js(user class file)
-      //then it push user.all =[]
-      
 
+      let newUser = new User(user)
       newUser.renderUsers()
       symptomNumber(user.symptoms,user)
-      // debugger
       
-       // we still need update userdiv after we moved renderUsers to user.js
-      // we get acces to return data with newUser.renderUsers()
-    // renderUsers(user)
-  // we get access to json data we are getting user array back (er can name it anything make sense)
-  // i am  getting mu users array and i am gonna get user array and object in the array an i rendered them indivudual 
+    
 
   }))
 }
 
 
-// function renderUsers(user){
-//   // console.log(users)
-  
-//   //  div.className ="card"
-//   // console.log(user)
-
-//    const userUl = document.createElement("ul")
-//    const userLi = document.createElement("li")
-//    userLi.innerText = `${user.name} ${user.lastname} ${user.age} ${user.gender} , Quarantined Start Date: ${user.quarantines[0].startdate}, Quarantined End Date: ${user.quarantines[0].enddate},  Symptoms fever: ${user.symptoms[0].fever}, cough: ${user.symptoms[0].cough}, tiredness: ${user.symptoms[0].breath}, throat: ${user.symptoms[0].throat}, Runny Nose:${user.symptoms[0].nose}`
-//    userUl.appendChild(userLi)
-//   // div.innerText = ""
-//   div.appendChild(userUl)
-  
-  
-   
-  
-// }
-
 function createFormHandler(e) {
   
   e.preventDefault()  
-  // let percentageNumber = 0 
+ 
   const name = document.querySelector('#input-name').value
   const lastname = document.querySelector('#input-lastname').value
   const age = document.querySelector('#input-age').value
@@ -71,8 +37,7 @@ function createFormHandler(e) {
   const other = document.querySelector("#input-description").value
   const startDate = document.querySelector("#start").value
   const endDate = document.querySelector("#end").value
-  // let displayNumberDiv = document.querySelector("#display-number")
-  // displayNumberDiv.innerHTML = percentageNumber
+  
   const bodyData = {
    name: name,
    lastname: lastname,
@@ -98,18 +63,11 @@ function createFormHandler(e) {
      enddate: endDate
    }],
 
-   
-    
- 
-
   }
-  postFetchUser(bodyData)
+  postFetchUser(bodyData) 
 }
 function postFetchUser (bodyData){
-
-  //build my body object outside of my fetch 
-
-  // console.log(bodyData)
+// console.log(bodyData)
 
 
   fetch("http://localhost:3000/users", {
@@ -117,10 +75,9 @@ function postFetchUser (bodyData){
     headers: { "Content-Type": "application/json"},
     body: JSON.stringify(bodyData)
   })
-  .then(response => response.json())
-  .then(user => {
-  console.log(user); /// when we are console.log we have to be sure shape of that what we are getting of user object i am intersted in  
-//  const bodyData = user  // when we console.log we are realize we are taking back user object when i am getting back object user object i am interested in 
+  .then(response => response.json()) 
+  .then(user => { 
+  // console.log(user); 
 let newUser = new User(user)
 newUser.renderUsers() 
  symptomNumber(bodyData.symptoms_attributes,user)
@@ -129,11 +86,9 @@ newUser.renderUsers()
 }
 function symptomNumber(symptoms,user){
   // console.log(symptoms)
-    
-  
     let yesCounter = 0
     for(let value in symptoms[0]){
-      //console.log(symptoms[0])
+      
       if(symptoms[0][value]=== 'Yes'){
        yesCounter ++
 
--- user.js
+++ user.js
@@ -1,12 +1,6 @@
-//console.log ("in this js")// confirm if we succesfully link to this.js file 
-// creating a constructor to give us all the attributes copys or prototype my user object 
-// i also wanna create an empty array to push all instances of "this" in to an empty array 
-// const div = document.querySelector(".users")
+const div = document.querySelector(".users")
 class User {
-  // this is just an object in globol scope must know about user class itself
  constructor(user){ 
-   //this-> instance some of the times 
-   // i am defining attributes on instances of the class 
   //  debugger
   this.id = user.id
   this.name = user.name
@@ -15,57 +9,21 @@ class User {
   this.gender = user.gender
   this.symptoms = user.symptoms
   this.quarantines = user.quarantines 
-
   // debugger
   
   User.all.push(this)
-  
-  
-   //each new instances i am going to push it this(user"first user") to User.all = []Array
-      
-      //then it push user.all =[]
   // debugger 
  }
 
- 
   renderUsers(){
     this.div = document.querySelector(".users")
     // debugger
-    // console.log(user)
     this.userUl = document.createElement("tr")
     this.userLi = document.createElement("td")
     this.userLi.innerText = `Name: ${this.name} Last Name: ${this.lastname} Age: ${this.age} Gender: ${this.gender} Quarantined Start Date: ${this.quarantines[0].startdate} Quarantined End Date: ${this.quarantines[0].enddate},  Symptoms fever: ${this.symptoms[0].fever}, cough: ${this.symptoms[0].cough}, tiredness: ${this.symptoms[0].breath}, throat: ${this.symptoms[0].throat}, Runny Nose:${this.symptoms[0].nose} Others: ${this.symptoms[0].other} ` 
     this.userUl.prepend(this.userLi)
     this.div.prepend(this.userUl)  
 }
-
-
-// renderUsers2(){
-
-//   this.div = document.querySelector(".users")
-//   // debugger
-//   // console.log(user)
-//   this.userUl = document.createElement("tr")
-
-//   this.userLi = document.createElement("td")
-//   this.userLi.innerText = `${this.name} `  
-//   this.userUl.appendChild(this.userLi)
-
-//   this.userLi2 = document.createElement("td")
-//   this.userLi2.innerText = `${this.lastname}  `  
-//   this.userUl.appendChild(this.userLi2)
-
-//   this.userLi3 = document.createElement("td")
-//   this.userLi3.innerText = ` ${this.age}`
-//   this.userUl.appendChild(this.userLi3)
-
-//   this.div.appendChild(this.userUl)  
-
-
-// }
- 
-
-
 
 }
 User.all = []
